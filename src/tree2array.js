8d774f20365fbb8cb20e2fb80686fc04d2e0d926
Yueming Fang <zergdo@gmail.com>
2022-11-24 16:18:32 +0800

    feat: delete children of tree2array()

--- src/tree2array.js
+++ src/tree2array.js
@@ -1,10 +1,13 @@
 const tree2array = (tree, options) => {
   if (!Array.isArray(tree)) return tree
+  const clone = JSON.parse(JSON.stringify(tree))
   options = { childrenKey: 'children', ...options }
   const { childrenKey } = options
-  return tree.flatMap(item => item[childrenKey]
+  const array = clone.flatMap(item => item[childrenKey]
     ? [item, ...tree2array(item[childrenKey], options)]
     : item)
+  array.forEach(a => delete a[childrenKey])
+  return array
 }
 
 module.exports = tree2array
--- test/index.js
+++ test/index.js
@@ -1,15 +1,21 @@
+const util = require('util')
+
 const { tree2array, array2tree, arrayParents } = require('../src')
 const tree = require('./tree.json')
 const array = require('./array.json')
 
 const treeArray = tree2array(tree)
-console.log('\ntree2array :|================> ' + treeArray.length)
-console.log(JSON.stringify(treeArray))
+console.log('\ntree2array: ===> ' + treeArray.length)
+console.log(util.inspect(treeArray, false, null, true))
 
 const arrayTree = array2tree(array)
-console.log('\narray2tree :|================> ' + arrayTree.length)
-console.log(JSON.stringify(arrayTree))
+console.log('\narray2tree: ===> ' + arrayTree.length)
+console.log(util.inspect(arrayTree, false, null, true))
+
+const treeArrayTree = array2tree(treeArray)
+console.log('\narray2tree -> tree2array: ===> ' + treeArrayTree.length)
+console.log(util.inspect(treeArrayTree, false, null, true))
 
 const parents = arrayParents('V1StGXR8', array)
-console.log('\narrayParents :|================> ' + parents.length)
-console.log(JSON.stringify(parents))
+console.log('\narrayParents: ===> ' + parents.length)
+console.log(util.inspect(parents, false, null, true))
